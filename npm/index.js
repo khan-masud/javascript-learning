7c014722fbe416d273ba5845234aeb08b1a2aeb8
Masud <abdullahalmasudkhan@gmail.com>
2024-08-25 01:57:16 +0600

    Added NPM

---
+++ npm/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (request, response)=> {
+    response.send('This is Masud.');
+})
+
+app.listen(1000, ()=> { // 1000 is port number. If you change this port, you have to restart the server.
+    console.log('Server is running on port 1000');
+})
